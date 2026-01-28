1210ae1219d9cab6d550b44915a3c85323007fe1
Rafiqul Islam <rafiqul.islam4@northsouth.edu>
2026-01-28 08:51:05 +0600

    up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -43,7 +43,8 @@ async function bootstrap() {
     }),
   );
   
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
